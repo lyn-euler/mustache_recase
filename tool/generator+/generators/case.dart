c9cf4526773846e06e3f0405400c086d7e019bc5
Rodsevich <nicorodsevich@gmail.com>
2017-10-02 02:44:24 -0300

    case generator fix

--- tool/generator+/generators/case.dart
+++ tool/generator+/generators/case.dart
@@ -7,32 +7,32 @@ class CaseGenerator extends Generator {
   String get description => "new case generator";
 
   Future execution() {
-    String snakeCaseName;
     askForInput("Provide the case name", "[a-z_A-Z]*").then((String name) {
       ReCase reCase = new ReCase(name);
-      variablesResolver["snake_case"] = snakeCaseName = reCase.snakeCase;
+      String snakeCaseName = reCase.snakeCase;
+      variablesResolver["snake_case"] = snakeCaseName;
       variablesResolver["camelCase"] = reCase.camelCase;
       variablesResolver["title"] = reCase.titleCase;
+      variablesResolver["escapeHack"] =
+          '{{ # $snakeCaseName }}{{test_var}} {{ \/ $snakeCaseName }}';
     });
     String assignmentString =
         '"{{snake_case}}": (LambdaContext ctx) => new ReCase(ctx.renderString()).{{camelCase}}';
-    processFile("lib/src/mustache_recase_base.dart",
-        templates: {"new-case": assignmentString});
-    variablesResolver["escapeHack"] =
-        '{{ # $snakeCaseName }}{{test_var}} {{ / $snakeCaseName }}';
     String sourceTemplate = 'testSource += "{{escapeHack}}";';
     String testTemplate = '''test("{{title}}", () {
       expect(compiled, contains(reCase.{{camelCase}}));
     });
     ''';
     String exampleTemplate =
-        'templateSource += "{{title}}: {{escapeHack}} \n";';
+        'templateSource += "{{title}}: {{escapeHack}} \\n";';
     processFile("example/cli_interactive_example.dart",
-        templates: {"new-case_source_template": exampleTemplate});
+        templates: {"new-case_example_template": exampleTemplate});
     processFile("test/mustache_recase_test.dart", templates: {
       "new-case_source_template": sourceTemplate,
       "new-case_test": testTemplate
     });
+    processFile("lib/src/mustache_recase_base.dart",
+        templates: {"new-case": assignmentString});
   }
 
   OverridingPolicy get overridePolicy => OverridingPolicy.NEVER;
