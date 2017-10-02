a596aacd741906ff99054c9728b7d9eae43ef06e
Rodsevich <nicorodsevich@gmail.com>
2017-10-02 02:28:15 -0300

    added example

---
+++ example/cli_interactive_example.dart
@@ -0,0 +1,20 @@
+import "dart:io";
+
+import 'package:mustache/mustache.dart';
+import 'package:mustache_recase/mustache_recase.dart' as mustache_recase;
+
+main(List<String> args) {
+  stdout.write("Provide a test string: ");
+  String testVar = stdin.readLineSync();
+
+  String templateSource = '';
+  //@generationAfter("new-case_example_template")
+  templateSource +=
+      "Camel Case: {{ # camel_case }}{{test_var}} {{ / camel_case }}\n";
+
+  Template template = new Template(templateSource);
+  Map variables = {"test_var": testVar};
+  variables.addAll(mustache_recase.cases);
+  String output = template.renderString(variables);
+  print(output);
+}
